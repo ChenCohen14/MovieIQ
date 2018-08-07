b0ae245818487ee33bee8ec8bc2898ae611c9874
ChenCohen14 <chencohen6890@gmail.com>
2018-08-07 14:08:09 +0300

    7.8 14:07

--- app/src/main/java/com/example/win10/movie_iq/TiersActivity.java
+++ app/src/main/java/com/example/win10/movie_iq/TiersActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -76,6 +77,11 @@ public class TiersActivity extends AppCompatActivity {
 //            Toast.makeText(this, "GO TO NEXT ACTIVITY", Toast.LENGTH_SHORT).show();
 //
 //        }
+    }
+
+    public void onLogout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 
 
