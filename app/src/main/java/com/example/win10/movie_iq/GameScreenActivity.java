a9b6b2daf481188fe95488f570acb3f76edd7884
ChenCohen14 <chencohen6890@gmail.com>
2018-08-07 18:18:17 +0300

    7.8 18:18

--- app/src/main/java/com/example/win10/movie_iq/GameScreenActivity.java
+++ app/src/main/java/com/example/win10/movie_iq/GameScreenActivity.java
@@ -1,13 +1,14 @@
 package com.example.win10.movie_iq;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,62 +17,127 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class GameScreenActivity extends AppCompatActivity {
 
     private static final String TAG = "GameScreenActivity";
 
     private TextView questionText;
+    private TextView hintText1;
+    private TextView hintText2;
+    private TextView hintText3;
+    private TextView factsText1;
+    private TextView factsText2;
+    private TextView factsText3;
+    private TextView tierText;
+    private TextView pointsText;
+    private EditText answerEditText;
     private Question theQuestion;
-//
-//    DatabaseReference databaseReference;
+    private Button hintBtn;
+    private Button submitBtn;
+    private Button factsBtn;
+    private String answer;
+    private boolean[] isHint = new boolean[3];
+    private boolean[] isFact = new boolean[3];
+    private ArrayList<String> solutions;
+
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        hintBtn = findViewById(R.id.hintButton);
+        submitBtn = findViewById(R.id.submitButton);
+        factsBtn = findViewById(R.id.raiseIQButton);
+        answerEditText = findViewById(R.id.answerEditText);
 
         String chosenBt = getIntent().getExtras().getString("chosenBt");
         theQuestion = (Question) getIntent().getSerializableExtra(chosenBt);
+        solutions = theQuestion.getSolutions();
 
         questionText = findViewById(R.id.questionTextView);
+        hintText1 = findViewById(R.id.hintTextView1);
+        hintText2 = findViewById(R.id.hintTextView2);
+        hintText3 = findViewById(R.id.hintTextView3);
+        factsText1 = findViewById(R.id.factTextView1);
+        factsText2 = findViewById(R.id.factTextView2);
+        factsText3 = findViewById(R.id.factTextView3);
+        tierText = findViewById(R.id.tierTextView);
+        pointsText = findViewById(R.id.pointTextView);
+
         questionText.setText(theQuestion.getQuestion());
+        tierText.setText("Tier " + Integer.toString(theQuestion.getTier()));
+        pointsText.setText("Points " + Integer.toString(theQuestion.getCurrentPoints()));
 
-//
-//        databaseReference = FirebaseDatabase.getInstance().getReference("questions");
-//
-//        Button getQ = findViewById(R.id.getQ);
-//        final Question[] q = {new Question()};
-//        getQ.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d(TAG, "getQ clicked");
-//               databaseReference.child("tier1").child("0").addListenerForSingleValueEvent(new ValueEventListener() {
-//                   @Override
-//                   public void onDataChange(DataSnapshot dataSnapshot) {
-//                       Log.d(TAG, "loading a question");
-//                       q[0] = dataSnapshot.getValue(Question.class);
-//                       Toast.makeText(GameScreenActivity.this, "GOT THE QUESTION", Toast.LENGTH_SHORT).show();
-//                       Log.d(TAG, "Question parsed is : " + q[0]);
-//                   }
-//
-//                   @Override
-//                   public void onCancelled(DatabaseError databaseError) {
-//
-//                   }
-//               });
-//            }
-//        });
-//
-//    }
+        hintBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //databaseReference = FirebaseDatabase.getInstance().getReference("questions");
 
-//    private void updateQuestion(){
-//        questionText = findViewById(R.id.questionTextView);
-//        questionText.setText(q.getQuestion());
-//
-//    }
+                //DatabaseReference condRef = databaseReference.child("tier"+Integer.toString(theQuestion.getTier())).child("0").child("currentPoints");
+
+                // condRef.setValue(5);
+
+                for (int i = 0; i < isHint.length; i++) {
+                    String hint = theQuestion.getHints().get(i);
+                    if (isHint[i] == false) {
+                        theQuestion.setCurrentPoints(theQuestion.getCurrentPoints() - 2);
+                        pointsText.setText("Points " + Integer.toString(theQuestion.getCurrentPoints()));
+                        if (i == 0)
+                            hintText1.setText(hint);
+                        if (i == 1)
+                            hintText2.setText(hint);
+                        if (i == 2)
+                            hintText3.setText(hint);
+                        isHint[i] = true;
+                        break;
+                    }
+                }
+            }
+        });
+
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer = answerEditText.getText().toString();
+                for (int i = 0; i < solutions.size(); i++) {
+                    if (answer.equalsIgnoreCase(solutions.get(i))) {
+                        Toast.makeText(getApplicationContext(), "Well Done!!", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+            }
+        });
+
+        factsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < solutions.size(); i++) {
+                    String fact = theQuestion.getFacts().get(i);
+                    if (isFact[i] == false) {
+                        if (i == 0) {
+                            factsText1.setText(fact);
+                            isFact[i] = true;
+                            break;
+                        }
+                        if (i == 1) {
+                            factsText2.setText(fact);
+                            isFact[i] = true;
+                            break;
+                        }
+                        if (i == 2) {
+                            factsText3.setText(fact);
+                            isFact[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
 
 //    public void onClick (View view){
 //
--- app/src/main/java/com/example/win10/movie_iq/Question.java
+++ app/src/main/java/com/example/win10/movie_iq/Question.java
@@ -12,15 +12,17 @@ public class Question implements Serializable {
     private int tier = 1;
     private ArrayList<String> hints = new ArrayList<>();
     private ArrayList<String> facts = new ArrayList<>();
+    private ArrayList<String> solutions = new ArrayList<>();
+
 
     public Question(){}
 
-    public Question (String question, String answer){
-        this.question = "In this animated coming of age classic, a lion cub will face family tragedy," +
-                " exile and the burden of leading his pride as king.";
-        this.answer = "The Lion King";
-
-    }
+//    public Question (String question, String answer){
+//        this.question = "In this animated coming of age classic, a lion cub will face family tragedy," +
+//                " exile and the burden of leading his pride as king.";
+//        this.answer = "The Lion King";
+//
+//    }
 
     public String getQuestion() {
         return question;
@@ -71,6 +73,22 @@ public class Question implements Serializable {
 
     public void setTier(int tier) {
         this.tier = tier;
+    }
+
+    public void setSolutions(ArrayList<String> solutions){
+        this.solutions = solutions;
+    }
+
+    public ArrayList<String> getSolutions() {
+        return solutions;
+    }
+
+    public ArrayList<String> getFacts() {
+        return facts;
+    }
+
+    public void setFacts(ArrayList<String> facts){
+        this.facts = facts;
     }
 
     @Override
